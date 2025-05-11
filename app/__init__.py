dabbc2fb283b58d92c11cd88cfee89137328f08d
Nynor <nynor@nfpires.com>
2025-05-11 20:42:48 +0100

    correction to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__)
     
     # Example config — replace with your actual DB URI
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@db:5432/yourdbname'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@meetingdb.cj20m40eazwi.eu-north-1.rds.amazonaws.com/meetings'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
